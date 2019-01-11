b1423300c97023360e0012b224118237d5c50a1a
Pavel <fandopaveel@gmail.com>
2019-01-11 20:40:26 +0300

    resolve problem with stack overflow

--- index.php
+++ index.php
@@ -10,38 +10,48 @@ $packages = [
     ],
     'C' => [
         'name' => 'C',
-        'dependencies' => ['B', 'D'],
+        'dependencies' => ['B','D'],
     ],
     'D' => [
         'name' => 'D',
-        'dependencies' => ['E'],
+        'dependencies' => ['A'],
     ],
     'E' => [
         'name' => 'E',
         'dependencies' => [],
     ],
 ];
-CheckKeys($packages);
-isDependenciesExist($packages);
-CheckDependencies($packages);//если предыдущая False, то ошибка
-$b = getAllPackageDependencies($packages, $argv[1]);
-print_r($b);
+validatePackageDefinitions($packages, $argv[1]);
+function validatePackageDefinitions(array $packages, string $scriptArgument)
+{
+    $emptyArray = [];
+    try {
+        CheckKeys($packages);
+        isDependenciesExist($packages);
+        CheckDependencies($packages);
+        print_r(getAllPackageDependencies($packages,$emptyArray,$scriptArgument));
+    } catch (Exception $e) {
+        echo 'Выброшено исключение: ', $e->getMessage(), "\n";
+    }
 
-function CheckKeys(array $packages)
+}
+
+function CheckKeys(array $packages): bool
 {
     foreach ($packages as $key => $value) {
-        //if (array_key_exists($value['name'], $packages))
-        if ($value['name'] === $key)
-            echo "$key \n";
+        if ($value['name'] !== $key) {
+            throw new Exception('Incorrect keys');
+        }
     }
+    return true;
 }
 
 function isDependenciesExist(array $packages)
 {
     foreach ($packages as $key => $value) {
-        $a = array_key_exists('dependencies', $packages[$key]);
-        if (!$a)
-            echo 'hui';
+        if (!array_key_exists('dependencies', $packages[$key])){
+            throw new Exception('There are elements without dependencies field');
+        }
     }
 
 }
@@ -51,20 +61,21 @@ function CheckDependencies(array $packages)
     foreach ($packages as $key => $value) {
         foreach (array_values($packages[$key]['dependencies']) as $value1) {
             if (!array_key_exists($value1, $packages))
-                echo $value1;
+                throw new Exception('Undefined dependencies');
         }
     }
 }
 
 // can be fatal error
-function getAllPackageDependencies(array $packages, string $packageName): array
+function getAllPackageDependencies(array $packages, array $arrayDependencies, string $packageName): array
 {
     $a = [];
     foreach (array_values($packages[$packageName]['dependencies']) as $value) {
+        if (in_array($value, $arrayDependencies))
+            throw new Exception('Cycle dependencies');
+            array_push($arrayDependencies, $value);
+            $a = getAllPackageDependencies($packages, $arrayDependencies, $value);
 
-        $a = getAllPackageDependencies($packages, $value);
     }
-
     return array_merge($a, $packages[$packageName]['dependencies']);
-
 }
